0e7e7c4fd8aadf1510bfc1de5e5a7d534b014136
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-08-27 16:50:47 +0200

    analysis

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+Traccia
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+
+BONUS 1:
+Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
+BONUS 2:
+Generiamo gli input da JS, invece di scriverli nel codice
+*/
+
+/*TO DO LIST
+1. Recupero gli elementi dal DOM.
+2. Stampare in pagina 5 numeri casuali.
+3. Creare una funzione che li rende visibili solo per 30 secondi.
+4. Alla fine dei 30 secondi appaiono 5 input di numeri(provo a iniettarli con JS direttamente, (Bonus1)).
+5. All'inserimento dei numeri, stabilire quali e quanti sono i numeri corretti inseriti dall'utente.
+6. Validazione. (Bonus2)
+*/
